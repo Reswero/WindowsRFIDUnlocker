4670b2e6046acfa92b93e92c4b5a27df44f1b82f
Reswero <reswero@mail.ru>
2022-11-27 16:25:51 +0600

    Добавление контроля доступа по карте и вход в систему

--- RFIDUnlocker.Hardware/RFIDUnlocker.Hardware.ino
+++ RFIDUnlocker.Hardware/RFIDUnlocker.Hardware.ino
@@ -1,7 +1,14 @@
 #include <MFRC522.h>
+#include <Keyboard.h>
 
 #define RFID_SS_Pin 10
 #define RFID_RST_Pin 2
+
+#define RED_LED_PIN 9
+#define GREEN_LED_PIN 6
+#define BLUE_LED_PIN 5
+
+#define MAX_LED_BRIGHTNESS 30
 
 MFRC522 RFID(RFID_SS_Pin, RFID_RST_Pin);
 
@@ -14,11 +21,23 @@ void setup() {
 
 void loop() {
   if (IsCardDetected()) {
+    String uid = "";
+    
     for (int i = 0; i < RFID.uid.size; i++) {
-      Serial.print(RFID.uid.uidByte[i], HEX);
-      Serial.print(" ");
+      uid += String(RFID.uid.uidByte[i], HEX);
     }
-    Serial.println();
+
+    uid.toUpperCase();
+    Serial.println(uid);
+
+    if (IsUidValid(uid)) {
+      GrantAccess();
+    }
+    else {
+      DenyAccess();
+    }
+
+    delay(2000);
   }
 }
 
@@ -32,4 +51,52 @@ bool IsCardDetected() {
   }
   
   return true;
+}
+
+bool IsUidValid(String uid) {
+  // TODO: проверка uid в базе
+  
+  if (uid == "") {
+    return true;
+  }
+
+  return false;
+}
+
+void GrantAccess() {
+  BlinkLed(GREEN_LED_PIN);
+  EnterPassword();
+}
+
+void DenyAccess() {
+  BlinkLed(RED_LED_PIN);
+}
+
+void BlinkLed(int ledPin) {
+  ResetAllLeds();
+
+  for (int i = 1; i < 3; i++) {
+    analogWrite(ledPin, MAX_LED_BRIGHTNESS);
+    delay(100);
+    analogWrite(ledPin, 0);
+    delay(100);
+  }
+}
+
+void ResetAllLeds() {
+  analogWrite(RED_LED_PIN, 0);
+  analogWrite(GREEN_LED_PIN, 0);
+  analogWrite(BLUE_LED_PIN, 0);
+}
+
+void EnterPassword() {
+  Keyboard.press(KEY_RETURN);
+  Keyboard.releaseAll();
+  delay(2000);
+
+  Keyboard.print("");
+  delay(100);
+
+  Keyboard.press(KEY_RETURN);
+  Keyboard.releaseAll();
 }
