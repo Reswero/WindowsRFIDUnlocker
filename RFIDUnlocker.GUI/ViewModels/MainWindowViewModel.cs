766e1df46e2b79a73213b0c537123b60c4edf361
Reswero <reswero@mail.ru>
2022-11-27 21:16:20 +0600

    Добавление вывода доступных COM-портов

---
+++ RFIDUnlocker.GUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO.Ports;
+using System.Runtime.CompilerServices;
+
+namespace RFIDUnlocker.GUI.ViewModels
+{
+	internal class MainWindowViewModel : INotifyPropertyChanged
+	{
+		public MainWindowViewModel()
+		{
+			COMPortNames = new(SerialPort.GetPortNames());
+		}
+
+		public ObservableCollection<string> COMPortNames { get; set; }
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
