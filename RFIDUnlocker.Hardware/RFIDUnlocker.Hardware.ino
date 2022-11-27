7621174d517df83e7341c2b3692afc9b65731696
Reswero <reswero@mail.ru>
2022-11-27 15:45:47 +0600

    Update RFIDUnlocker.Hardware.ino
    Добавление вывода идентификатора карты

--- RFIDUnlocker.Hardware/RFIDUnlocker.Hardware.ino
+++ RFIDUnlocker.Hardware/RFIDUnlocker.Hardware.ino
@@ -1,9 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <MFRC522.h>
 
+#define RFID_SS_Pin 10
+#define RFID_RST_Pin 2
+
+MFRC522 RFID(RFID_SS_Pin, RFID_RST_Pin);
+
+void setup() {
+  Serial.begin(9600);
+
+  SPI.begin();
+  RFID.PCD_Init();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (IsCardDetected()) {
+    for (int i = 0; i < RFID.uid.size; i++) {
+      Serial.print(RFID.uid.uidByte[i], HEX);
+      Serial.print(" ");
+    }
+    Serial.println();
+  }
+}
 
+bool IsCardDetected() {
+  if (!RFID.PICC_IsNewCardPresent()) {
+    return false;
+  }
+
+  if (!RFID.PICC_ReadCardSerial()) {
+    return false;
+  }
+  
+  return true;
 }
