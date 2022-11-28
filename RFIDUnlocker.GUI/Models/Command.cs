4010de8b8c40de8d0627b07be10f2cf3d5a163e8
Reswero <reswero@mail.ru>
2022-11-28 10:05:57 +0600

    Добавление команд
    Команда подключения к COM-порту и  команда добавления новой карты

---
+++ RFIDUnlocker.GUI/Models/Command.cs
@@ -0,0 +1,12 @@
+﻿namespace RFIDUnlocker.GUI.Models
+{
+	internal static class Command
+	{
+		public const string Add = "add";
+		public const string Delete = "del";
+		public const string Update = "upd";
+		public const string GetAllCards = "gac";
+		public const string Log = "log";
+		public const string SetPassword = "psw";
+	}
+}
--- RFIDUnlocker.GUI/ViewModels/MainWindowViewModel.cs
+++ RFIDUnlocker.GUI/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.ObjectModel;
+﻿using RFIDUnlocker.GUI.Infrastructure.Commands;
+using RFIDUnlocker.GUI.Models;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO.Ports;
 using System.Runtime.CompilerServices;
@@ -12,7 +14,50 @@ namespace RFIDUnlocker.GUI.ViewModels
 			COMPortNames = new(SerialPort.GetPortNames());
 		}
 
+		private SerialPort _serialPort;
+
 		public ObservableCollection<string> COMPortNames { get; set; }
+		public string SelectedCOMPortName { get; set; }
+
+		#region
+
+		private RelayCommand _connectCOMPort;
+		public RelayCommand ConnectCOMPort 
+			=> _connectCOMPort ??= new(_ =>
+			{
+				if (SelectedCOMPortName != null)
+				{
+					_serialPort = new()
+					{
+						PortName = SelectedCOMPortName,
+						BaudRate = 9600,
+						DataBits = 8,
+						ReadTimeout = 500,
+						WriteTimeout = 500
+					};
+				}
+			});
+
+		private RelayCommand _addCard;
+		public RelayCommand AddCard
+			=> _addCard ??= new(_ =>
+			{
+				if (_serialPort != null)
+				{
+					SendRequest(Command.Add);
+				}
+			});
+
+		#endregion
+
+		private void SendRequest(string command)
+		{
+			string request = $"<?{command}>";
+
+			_serialPort.Open();
+			_serialPort.Write(request);
+			_serialPort.Close();
+		}
 
 		public event PropertyChangedEventHandler? PropertyChanged;
 
